f0d8a3868ff116e9017f62f432d67bd0198372da
XmasLogos <Xmas@logos.ren>
2025-02-28 11:39:13 +0800

    Update providers.tf

--- TerraTest/providers.tf
+++ TerraTest/providers.tf
@@ -1,3 +1,7 @@
+data "azurerm_subscription" "current" {
+
+}
+
 terraform {
   required_version = ">= 1.1.0"
   required_providers {
@@ -6,13 +10,13 @@ terraform {
       version = "4.20.0"
     }
   }
-#   backend "azurerm" {
-#     subscription_id = "value"
-#     resource_group_name  = "Xmas-Own"
-#     storage_account_name = "xstoragesea001"
-#     container_name       = "terraform-state"
-#     key                  = "terraform.tfstate"
-#   }
+  backend "azurerm" {
+    subscription_id = data.azurerm_subscription.current.subscription_id
+    resource_group_name  = "Xmas-Own"
+    storage_account_name = "xstoragesea001"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
